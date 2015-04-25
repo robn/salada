65f55fd8331995e3fcf9984fc99f554b005b37d9
Robert Norris <rob@eatenbyagrue.org>
2015-04-25 15:15:14 +1000

    Basic web server, accepts JMAP methods and parses them

--- src/salada.rs
+++ src/salada.rs
@@ -1,4 +1,58 @@
+extern crate hyper;
+extern crate rustc_serialize;
 extern crate jmap;
 
+use hyper::server::{Request, Response};
+use hyper::method::Method::Post;
+use hyper::status::StatusCode;
+use hyper::uri::RequestUri::AbsolutePath;
+use hyper::header;
+
+use rustc_serialize::json::Json;
+use jmap::util::FromJson;
+use jmap::method::RequestMethod;
+
+fn http_handler(mut req: Request, mut res: Response) {
+    res.headers_mut().set(header::Server("salada/0.0.1".to_string()));
+
+    let uri = req.uri.clone();
+
+    *res.status_mut() = match uri {
+        AbsolutePath(ref path) => match (&req.method, &path[..]) {
+
+            (&Post, "/jmap") => {
+                match Json::from_reader(&mut req) {
+                    Ok(j) => match RequestMethod::from_json(&j) {
+                        Ok(m) => {
+                            println!("method: {:?}", m);
+                            StatusCode::Ok
+                        },
+                        Err(e) => {
+                            println!("jmap parse error: {}", e);
+                            StatusCode::BadRequest
+                        },
+                    },
+                    Err(e) => {
+                        println!("json parse error: {}", e);
+                        StatusCode::BadRequest
+                    },
+                }
+            },
+
+            (_, "/jmap") => StatusCode::MethodNotAllowed,
+            _            => StatusCode::NotFound,
+        },
+        _ => StatusCode::BadRequest,
+    };
+
+    match res.start().and_then(|res| res.end()) {
+        Err(e) => println!("response error: {}", e),
+        _      => (),
+    }
+}
+
 fn main() {
+    let server = hyper::Server::http(http_handler);
+    let _listen_guard = server.listen("127.0.0.1:3000").unwrap();
+    println!("Listening on http://127.0.0.1:3000/jmap");
 }
