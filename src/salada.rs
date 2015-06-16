656c8fb2c12fc6436d175ee9c7cd643cfa451ea8
Robert Norris <rob@eatenbyagrue.org>
2015-06-16 17:05:27 +1000

    Use a macro to build the CRUD dispatcher

--- src/salada.rs
+++ src/salada.rs
@@ -11,11 +11,6 @@ mod logger;
 mod db;
 mod util;
 mod record;
-mod calendar;
-mod calendarevent;
-mod contact;
-mod contactgroup;
-mod mailbox;
 
 use std::default::Default;
 use std::io::Write;
@@ -34,12 +29,30 @@ use jmap::method::RequestMethod::*;
 use jmap::method::ResponseMethod::*;
 
 use util::RequestContext;
-use calendar::CalendarHandler;
-use calendarevent::CalendarEventHandler;
-use contact::ContactHandler;
-use contactgroup::ContactGroupHandler;
-use mailbox::MailboxHandler;
+use record::RecordHandler;
 use db::Db;
+
+
+macro_rules! make_crud_method_dispatcher {
+    ($method: expr, $rmethods: expr, $r: expr,
+     $($ty: ty { $get: ident => $rget: ident, $set: ident => $rset: ident, $getup: ident => $rgetup: ident }),*) => {
+        $rmethods = match $method {
+            $(
+                $get(args, id)
+                    => vec!($r.get_records(&args).map(|a| $rget(a, id.clone())).unwrap_or_else(|e| ResponseError(e, id.clone()))),
+                $set(args, id)
+                    => vec!($r.set_records(&args).map(|a| $rset(a, id.clone())).unwrap_or_else(|e| ResponseError(e, id.clone()))),
+                $getup(args, id) => $r.get_record_updates(&args).map(|a| {
+                    match a {
+                        (a, Some(b)) => vec!($rgetup(a, id.clone()), $rget(b, id.clone())),
+                        (a, _)       => vec!($rgetup(a, id.clone())),
+                    }
+                }).unwrap_or_else(|e| vec!(ResponseError(e, id.clone()))),
+            )*
+            RequestError(args, id) => vec!(ResponseError(args, id)),
+        };
+    }
+}
 
 
 fn jmap_handler(batch: RequestBatch) -> ResponseBatch {
@@ -51,64 +64,34 @@ fn jmap_handler(batch: RequestBatch) -> ResponseBatch {
     };
 
     for method in batch.0.into_iter() {
-        let rmethods: Vec<ResponseMethod> = match method {
-            GetCalendars(args, id)
-                => vec!(r.get_calendars(&args).map(|a| Calendars(a, id.clone())).unwrap_or_else(|e| ResponseError(e, id.clone()))),
-            SetCalendars(args, id)
-                => vec!(r.set_calendars(&args).map(|a| CalendarsSet(a, id.clone())).unwrap_or_else(|e| ResponseError(e, id.clone()))),
-            GetCalendarUpdates(args, id) => r.get_calendar_updates(&args).map(|a| {
-                match a {
-                    (a, Some(b)) => vec!(CalendarUpdates(a, id.clone()), Calendars(b, id.clone())),
-                    (a, _)       => vec!(CalendarUpdates(a, id.clone())),
-                }
-            }).unwrap_or_else(|e| vec!(ResponseError(e, id.clone()))),
-
-            GetCalendarEvents(args, id)
-                => vec!(r.get_calendar_events(&args).map(|a| CalendarEvents(a, id.clone())).unwrap_or_else(|e| ResponseError(e, id.clone()))),
-            SetCalendarEvents(args, id)
-                => vec!(r.set_calendar_events(&args).map(|a| CalendarEventsSet(a, id.clone())).unwrap_or_else(|e| ResponseError(e, id.clone()))),
-            GetCalendarEventUpdates(args, id) => r.get_calendar_event_updates(&args).map(|a| {
-                match a {
-                    (a, Some(b)) => vec!(CalendarEventUpdates(a, id.clone()), CalendarEvents(b, id.clone())),
-                    (a, _)       => vec!(CalendarEventUpdates(a, id.clone())),
-                }
-            }).unwrap_or_else(|e| vec!(ResponseError(e, id.clone()))),
-
-            GetContacts(args, id)
-                => vec!(r.get_contacts(&args).map(|a| Contacts(a, id.clone())).unwrap_or_else(|e| ResponseError(e, id.clone()))),
-            SetContacts(args, id)
-                => vec!(r.set_contacts(&args).map(|a| ContactsSet(a, id.clone())).unwrap_or_else(|e| ResponseError(e, id.clone()))),
-            GetContactUpdates(args, id) => r.get_contact_updates(&args).map(|a| {
-                match a {
-                    (a, Some(b)) => vec!(ContactUpdates(a, id.clone()), Contacts(b, id.clone())),
-                    (a, _)       => vec!(ContactUpdates(a, id.clone())),
-                }
-            }).unwrap_or_else(|e| vec!(ResponseError(e, id.clone()))),
-
-            GetContactGroups(args, id)
-                => vec!(r.get_contactgroups(&args).map(|a| ContactGroups(a, id.clone())).unwrap_or_else(|e| ResponseError(e, id.clone()))),
-            SetContactGroups(args, id)
-                => vec!(r.set_contactgroups(&args).map(|a| ContactGroupsSet(a, id.clone())).unwrap_or_else(|e| ResponseError(e, id.clone()))),
-            GetContactGroupUpdates(args, id) => r.get_contactgroup_updates(&args).map(|a| {
-                match a {
-                    (a, Some(b)) => vec!(ContactGroupUpdates(a, id.clone()), ContactGroups(b, id.clone())),
-                    (a, _)       => vec!(ContactGroupUpdates(a, id.clone())),
-                }
-            }).unwrap_or_else(|e| vec!(ResponseError(e, id.clone()))),
-
-            GetMailboxes(args, id)
-                => vec!(r.get_mailboxes(&args).map(|a| Mailboxes(a, id.clone())).unwrap_or_else(|e| ResponseError(e, id.clone()))),
-            SetMailboxes(args, id)
-                => vec!(r.set_mailboxes(&args).map(|a| MailboxesSet(a, id.clone())).unwrap_or_else(|e| ResponseError(e, id.clone()))),
-            GetMailboxUpdates(args, id) => r.get_mailbox_updates(&args).map(|a| {
-                match a {
-                    (a, Some(b)) => vec!(MailboxUpdates(a, id.clone()), Mailboxes(b, id.clone())),
-                    (a, _)       => vec!(MailboxUpdates(a, id.clone())),
-                }
-            }).unwrap_or_else(|e| vec!(ResponseError(e, id.clone()))),
-
-            RequestError(args, id) => vec!(ResponseError(args, id)),
-        };
+        let rmethods: Vec<ResponseMethod>;
+        make_crud_method_dispatcher!(method, rmethods, r,
+            Calendar {
+                GetCalendars       => Calendars,
+                SetCalendars       => CalendarsSet,
+                GetCalendarUpdates => CalendarUpdates
+            },
+            CalendarEvent {
+                GetCalendarEvents       => CalendarEvents,
+                SetCalendarEvents       => CalendarEventsSet,
+                GetCalendarEventUpdates => CalendarEventUpdates
+            },
+            Contact {
+                GetContacts       => Contacts,
+                SetContacts       => ContactsSet,
+                GetContactUpdates => ContactUpdates
+            },
+            ContactGroup {
+                GetContactGroups       => ContactGroups,
+                SetContactGroups       => ContactGroupsSet,
+                GetContactGroupUpdates => ContactGroupUpdates
+            },
+            Mailbox {
+                GetMailboxes      => Mailboxes,
+                SetMailboxes      => MailboxesSet,
+                GetMailboxUpdates => MailboxUpdates
+            }
+        );
 
         rbatch.0.extend(rmethods.into_iter());
     }
